75d845f0689653515d01c57c123efd61f2a3847b
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-04 02:13:41 +0000

    [Finish]2-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top' #画面を表示したい」のでGET どのURLにアクセスしたときに、「どのコントローラのどのアクションを呼び出すか」を設定
 end
